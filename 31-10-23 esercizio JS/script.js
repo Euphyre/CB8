4601f2f9f4ce1602f6318eb8503abc4e604b0b50
Euphyre <148492199+Euphyre@users.noreply.github.com>
2023-11-01 19:45:45 +0100

    Add files via upload

---
+++ 31-10-23 esercizio JS/script.js
@@ -0,0 +1,16 @@
+const animalName = [
+  "Cat",
+  "Lion",
+  "Monkey",
+  "Parrot",
+  "Dog",
+  "Frog",
+  "Hawk",
+  "Horse",
+];
+
+console.log(animalName);
+
+for (let i = 0; i <= animalName.length - 1; i++) {
+  console.log(i + 1 + ":" + animalName[i]);
+}
